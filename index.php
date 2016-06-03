f741d729ee37ad7b56e78e29035af2567a395fcf
James Jester <elmojester@gmail.com>
2016-06-02 20:55:27 -0500

    Added new images.

---
+++ index.php
@@ -0,0 +1,52 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+            <link rel="stylesheet" href="styles.css">
+        </head>
+        <body id='final-example'>
+            
+            <div class='wrapper'>
+                
+                <div id='banner'>
+                    
+                </div>
+                
+                <div id='nav'>
+                    
+                </div>
+                
+                <div class='content'>
+                    
+                    <div id='footer' class='cf'>
+                        
+                        <div class='column three'>
+                            <strong>Phone</strong>
+                            808.529.3819
+                        </div>
+                        
+                        <div class='column three'>
+                            <strong>Location</strong>
+                            123 Kapiolani Blvd
+                            Honolulu, HI
+                        </div>
+                        
+                        <div class='column three last'>
+                            <strong>Hours</strong>
+                            <em>Tuesday - Thursday</em>
+                            1:00pm - 9:00pm
+                            
+                            <em>Friday - Saturday</em>
+                            4:00pm - 11:00pm
+                            
+                            <em>Sunday - Monday</em>
+                            Closed
+                            
+                        </div>
+                    </div>
+                        <small>&copy; 2014 Juicy J</small>
+                </div>
+            </div>
+            
+        </body>
+    </html>
