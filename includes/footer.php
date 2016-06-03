bf8b9b383ec50522d450f1747408418d17c1b7ec
James Jester <elmojester@gmail.com>
2016-06-03 18:06:47 -0500

    Added new folder with templates.

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<div id='footer' class='cf'>
+                        
+                        <div class='column three'>
+                            <strong>Phone</strong>
+                            808.529.3819
+                        </div>
+                        
+                        <div class='column three'>
+                            <strong>Location</strong>
+                            123 Kapiolani Blvd
+                            Honolulu, HI
+                        </div>
+                        
+                        <div class='column three last'>
+                            <strong>Hours</strong>
+                            <em>Tuesday - Thursday</em>
+                            1:00pm - 9:00pm
+                            
+                            <em>Friday - Saturday</em>
+                            4:00pm - 11:00pm
+                            
+                            <em>Sunday - Monday</em>
+                            Closed
+                            
+                        </div>
+                    </div>
+                        <small>&copy; 2014 Juicy J</small>
+                </div>
+            </div>
+            
+        </body>
+    </html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <title></title>
+            <link rel="stylesheet" href="styles.css">
+        </head>
+        <body id='final-example'>
+            
+            <div class='wrapper'>
+                
+                <div id='banner'>
+                    <a href="/" title='Return to Home'></a>
+                    <img src="img/image_71.jpeg" alt="Pizza Place">
+                </div>
+                
+                <div id='nav'>
+                   <?php include('includes/nav.php'); ?>
+                </div>
+                
+                <div class='content'>
