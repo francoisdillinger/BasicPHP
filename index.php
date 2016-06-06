f1ca694d3d5558c9f477d92a8584c22fb8fcb859
James Jester <elmojester@gmail.com>
2016-06-05 21:26:20 -0500

    Updating index.

--- index.php
+++ index.php
@@ -1,58 +1,6 @@
-<DOCTYPE html>
-    <html>
-        <head>
-            <title></title>
-            <link rel="stylesheet" href="styles.css">
-        </head>
-        <body id='final-example'>
-            
-            <div class='wrapper'>
-                
-                <div id='banner'>
-                    <a href="/" title='Return to Home'></a>
-                    <img src="img/image_71.jpeg" alt="Pizza Place">
-                </div>
-                
-                <div id='nav'>
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="team.php">Team</a></li>
-                        <li><a href="menu.php">Menu</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
-                </div>
-                
-                <div class='content'>
-                    
-                    <div id='footer' class='cf'>
-                        
-                        <div class='column three'>
-                            <strong>Phone</strong>
-                            808.529.3819
-                        </div>
-                        
-                        <div class='column three'>
-                            <strong>Location</strong>
-                            123 Kapiolani Blvd
-                            Honolulu, HI
-                        </div>
-                        
-                        <div class='column three last'>
-                            <strong>Hours</strong>
-                            <em>Tuesday - Thursday</em>
-                            1:00pm - 9:00pm
-                            
-                            <em>Friday - Saturday</em>
-                            4:00pm - 11:00pm
-                            
-                            <em>Sunday - Monday</em>
-                            Closed
-                            
-                        </div>
-                    </div>
-                        <small>&copy; 2014 Juicy J</small>
-                </div>
-            </div>
-            
-        </body>
-    </html>
+<?php
+    include('includes/header.php');
+?>
+<?php
+    include('includes/footer.php');
+?>
